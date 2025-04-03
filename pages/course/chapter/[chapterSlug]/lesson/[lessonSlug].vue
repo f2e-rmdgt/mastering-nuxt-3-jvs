2cb84992c9f2418e5571a7c08a80d82c4600698e
gotop1711 <gotop1711@gmail.com>
2025-04-03 16:38:43 +0800

    Revert "jvs_3-4_test: create server-side errors as routes not existing"
    This reverts commit 72791a95464cdb827ecaac235f54560f5af1b041.

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -74,26 +74,11 @@ const chapter = computed(() => {
     (chapter) => chapter.slug === route.params.chapterSlug
   );
 });
-
-if (!chapter.value) {
-  throw createError({
-    statusCode: 404,
-    message: 'Chapter not found',
-  });
-}
-
 const lesson = computed(() => {
   return chapter.value.lessons.find(
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
-
-if (!lesson.value) {
-  throw createError({
-    statusCode: 404,
-    message: 'Lesson not found',
-  });
-}
 
 const title = computed(() => {
   return `${lesson.value.title} - ${course.title}`;
